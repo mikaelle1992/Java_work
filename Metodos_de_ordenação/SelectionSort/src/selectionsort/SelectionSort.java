c07eec307f00e78e7f01d36089cb0837c60da749
mikaelle <mikaelle.rubiaps02@gmail.com>
2021-04-26 19:32:58 -0300

    codigo e explicação do selectionSort

---
+++ Metodos_de_ordenação/SelectionSort/src/selectionsort/SelectionSort.java
@@ -0,0 +1,43 @@
+package selectionsort;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+  
+    public static void main(String[] args) {
+         int troca, menor, N;
+        Scanner tec = new Scanner(System.in);
+        
+        System.out.println("Tamanho do vetor");
+        N = tec.nextInt();
+    
+        int [] vetor = new int [N];
+        
+         for(int i = 0;i< vetor.length;i++){
+            System.out.println("valor "+(i+1)+ " do vetor.");
+            int valor = tec.nextInt();  
+            vetor[i] = valor;
+        }
+        
+        for(int i = 0; i <N-1; i++){
+            menor =i;
+            for(int j = i+1; j<N; j++){
+                if(vetor[j] < vetor[menor]){
+                    menor = j;
+                }if(i != menor){
+                    troca = vetor[i];
+                    vetor[i] = vetor[menor];
+                    vetor[menor] = troca;
+                }
+            }
+            
+        }
+
+          for(int i = 0;i< vetor.length;i++){
+            System.out.println("valores ordenado : " + vetor[i]);
+         }
+        
+    }
+    
+}

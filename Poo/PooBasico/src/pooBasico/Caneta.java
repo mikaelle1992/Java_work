038440e7005b943647c6014f06503a1079f77746
mikaelle <mikaelle.rubiaps02@gmail.com>
2021-05-01 09:58:06 -0300

    poo basico

--- Poo/PooBasico/src/pooBasico/Caneta.java
+++ Poo/PooBasico/src/pooBasico/Caneta.java
@@ -3,30 +3,111 @@ package pooBasico;
 
 
 public class Caneta {
-   public String modelo;
-   public String cor;
+   private String modelo;
+   private String cor;
    private float ponta;
-   protected int carga; 
-   public  boolean tampada;
+   private int carga; 
+   private  boolean tampada;
   
-    public void status(){
-        System.out.println("Caneta: " +this.modelo +", \nCor: "+this.cor+", \nPonta : "+this.ponta+",\nCarga :"+ this. carga);
+   public  Caneta(String modelo,String cor,float ponta){
+       this.setModelo(modelo);
+       this.setCor(cor);
+       this.setPonta(getPonta());
+       this.setCarga(80);
+       this.setTampada(true);
+       rabiscar();
+ 
+   }
+  
+   public void tampar(){
+        setTampada(true);
+    }
+    public void destampar(){
+        setTampada(false);
+    }
+  
+
+    /**
+     * @return the modelo
+     */
+    public String getModelo() {
+        return modelo;
+    }
+
+    /**
+     * @param modelo the modelo to set
+     */
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    /**
+     * @return the cor
+     */
+    public String getCor() {
+        return cor;
+    }
+
+    /**
+     * @param cor the cor to set
+     */
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    /**
+     * @return the ponta
+     */
+    public float getPonta() {
+        return ponta;
+    }
+
+    /**
+     * @param ponta the ponta to set
+     */
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    /**
+     * @return the carga
+     */
+    public int getCarga() {
+        return carga;
+    }
+
+    /**
+     * @param carga the carga to set
+     */
+    public void setCarga(int carga) {
+        this.carga = carga;
+    }
+
+    /**
+     * @return the tampada
+     */
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    /**
+     * @param tampada the tampada to set
+     */
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
     }
     
-    public void ribiscar(){
-        if(tampada){
+    
+      public void rabiscar(){
+        if(isTampada()){
             System.out.println("erro!, destampe a caneta");
         }else{
             System.out.println("Rabiscando....");
         }
+      }
+           public void status(){
+        System.out.println("Caneta: " +this.getModelo() +", \nCor: "+this.getCor()+", \nPonta : "+this.getPonta()+",\nCarga :"+ this.getCarga());
     }
-    
-   public void tampar(){
-        tampada = true;
-    }
-    public void destampar(){
-        tampada = false;
-    }
-    
+        
     
 }
--- Poo/PooBasico/src/pooBasico/PooBasico.java
+++ Poo/PooBasico/src/pooBasico/PooBasico.java
@@ -3,18 +3,11 @@ package pooBasico;
 public class PooBasico {
     
     public static void main(String[] args) {
-        Caneta c1 = new Caneta();
-        
-        c1.modelo = "bic";
-        c1.cor = "azul";
-      //  c1.ponta = 0.6f;
-        c1.carga = 80;
-        c1.tampada = false;
+       Caneta c1 = new Caneta("big","azul",0.4f);
+       c1.status();
+       c1.destampar();
      
-        c1.status();
-        c1.ribiscar();
-        c1.tampar();
-        c1.ribiscar();
+       
     }
     
 }

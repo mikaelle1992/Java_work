61b2426412dadd5914d2a7b178ef337e33993b58
mikaelle rubia <mikaellerubiaps02@gmail.com>
2021-05-11 21:11:10 -0300

    criado dos atributos

---
+++ Poo/RelacionClass/src/relacionclass/Lutador.java
@@ -0,0 +1,29 @@
+
+package relacionclass;
+
+
+public class Lutador {
+    private String nome; 
+    private String nacionalidade;
+    private int idade;
+    private float altura;
+    private float peso;
+    private String Categoria;
+    private int vitoria, derotas, empates;
+   
+    public void apresentar(){
+        
+    }
+    
+      public void status(){
+        
+    }
+      
+        public void ganharLuta(){
+        
+    }
+        
+         public void empatarLuta(){
+        
+    }
+}
---
+++ Poo/RelacionClass/src/relacionclass/RelacionClass.java
@@ -0,0 +1,17 @@
+
+package relacionclass;
+
+/**
+ *
+ * @author Mikaelle
+ */
+public class RelacionClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
